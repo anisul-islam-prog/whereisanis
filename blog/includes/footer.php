1952e0356c3b360f4aee38d4f24f06b0aac3cc95
anisul-islam-prog <kingfahad0802@gmail.com>
2022-04-02 21:28:18 +0600

    blog module added

---
+++ blog/includes/footer.php
@@ -0,0 +1,17 @@
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10 col-lg-8 mx-auto">
+                <ul class="list-inline text-center">
+                    <li class="list-inline-item"><a href="https://www.linkedin.com/in/whoisanis/" target="_blank"><span
+                                    class="fa-stack fa-lg"><i class="fa fa-circle fa-stack-2x"></i><i
+                                        class="fa fa-linkedin fa-stack-1x fa-inverse"></i></span></a></li>
+                    <li class="list-inline-item"><a href="https://github.com/anisul-islam-prog" target="_blank"><span
+                                    class="fa-stack fa-lg"><i class="fa fa-circle fa-stack-2x"></i><i
+                                        class="fa fa-github fa-stack-1x fa-inverse"></i></span></a></li>
+                </ul>
+                <p class="text-muted copyright">Copyright&nbsp;©&nbsp;Anis 2022</p>
+            </div>
+        </div>
+    </div>
+</footer>

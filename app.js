ab8a2b78bbc5d2dec20a477af3c9be12f1d73240
anisul-islam-prog <kingfahad0802@gmail.com>
2022-05-16 10:02:09 +0600

    Section Position changed. Default: Dark Mode

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ function PageTransitions(){
     const themeBtn = document.querySelector('.theme-btn');
     themeBtn.addEventListener('click',() =>{
         let element = document.body;
-        element.classList.toggle('dark-mode')
+        element.classList.toggle('light-mode')
     })
 }
 
--- blog/admin/includes/config.php
+++ blog/admin/includes/config.php
@@ -1,9 +1,9 @@
 <?php
 // DB credentials.
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'blog');
+define('DB_HOST', 'sql107.epizy.com');
+define('DB_USER', 'epiz_31434967');
+define('DB_PASS', 'N6xRA4qJMyAwBLJ');
+define('DB_NAME', 'epiz_31434967_blog');
 // Establish database connection.
 try {
     $dbh = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASS, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));

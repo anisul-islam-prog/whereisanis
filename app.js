37eeecc5f420c43bd42bee15bb580526705e53f1
anisul-islam-prog <kingfahad0802@gmail.com>
2022-05-16 15:24:33 +0600

    Design changes. Content Updated

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ const sectBtn = document.querySelectorAll('.control');
 const allSections = document.querySelector('.main-content');
 
 
-function PageTransitions(){
+function PageTransitions() {
     //Button click active class
-    for(let i = 0; i < sectBtn.length; i++){
-        sectBtn[i].addEventListener('click', function(){
+    for (let i = 0; i < sectBtn.length; i++) {
+        sectBtn[i].addEventListener('click', function () {
             let currentBtn = document.querySelectorAll('.active-btn');
             currentBtn[0].className = currentBtn[0].className.replace('active-btn', '');
             this.className += ' active-btn';
@@ -15,17 +15,17 @@ function PageTransitions(){
     }
 
     //Sctions Active 
-    allSections.addEventListener('click', (e) =>{
+    allSections.addEventListener('click', (e) => {
         const id = e.target.dataset.id;
-        if(id){
+        if (id) {
             //resmove selected from the other btns
-            sectBtns.forEach((btn) =>{
+            sectBtns.forEach((btn) => {
                 btn.classList.remove('active')
             })
             e.target.classList.add('active')
 
             //hide other sections
-            sections.forEach((section)=>{
+            sections.forEach((section) => {
                 section.classList.remove('active')
             })
 
@@ -36,10 +36,58 @@ function PageTransitions(){
 
     //Toggle theme
     const themeBtn = document.querySelector('.theme-btn');
-    themeBtn.addEventListener('click',() =>{
+    themeBtn.addEventListener('click', () => {
         let element = document.body;
         element.classList.toggle('light-mode')
     })
 }
 
 PageTransitions();
+
+var isLast = function (word) {
+    return $(word).next().length > 0 ? false : true;
+}
+
+var getNext = function (word) {
+    return $(word).next();
+}
+
+var getVisible = function () {
+    return document.getElementsByClassName('is-visible');
+}
+
+var getFirst = function () {
+    var node = $('.words-wrapper').children().first();
+    return node;
+}
+
+var switchWords = function (current, next) {
+    $(current).removeClass('is-visible').addClass('is-hidden');
+    $(next).removeClass('is-hidden').addClass('is-visible');
+}
+
+var getStarted = function () {
+    //We start by getting the visible element and its sibling
+    var first = getVisible();
+    var next = getNext(first);
+
+    //If our element has a sibling, it's not the last of the list. We switch the classes
+    if (next.length !== 0) {
+        switchWords(first, next);
+    } else {
+
+        //The element is the last of the list. We remove the visible class of the current element
+        $(first).removeClass('is-visible').addClass('is-hidden');
+
+        //And we get the first element of the list, and we give it the visible class. And it starts again.
+        var newEl = getFirst();
+        $(newEl).removeClass('is-hidden').addClass('is-visible');
+    }
+
+}
+
+var init = function () {
+    setInterval(function () { getStarted() }, 2000);
+}
+
+init();
